91bbffe3cb97041d9d0a2a918a0aeec791a4e121
NA <NA>
2023-08-07 14:39:31 -0400

    Raw mode enabled Raw mode and basic error handling implementation.

--- main.c
+++ main.c
@@ -1,29 +1,66 @@
+/* Includes */
+
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <errno.h>
+
+/* Data */
 
 struct termios original_attributes;
 
+/* Terminal */
+
+void killProgram(const char* s) {
+    perror(s);
+    exit(1);
+}
+
 void disableRawmode() {
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_attributes);
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_attributes) == -1)
+        killProgram("tcsetattr");
 }
 
 void enableRawmode() {
-    tcgetattr(STDIN_FILENO, &original_attributes);
+    if(tcgetattr(STDIN_FILENO, &original_attributes) == -1) 
+        killProgram("tcgetattr");
+    
     atexit(disableRawmode);
 
     struct termios raw_attributes = original_attributes;
 
-    raw_attributes.c_lflag &= ~(ECHO | ICANON);
-    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_attributes);
+    raw_attributes.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw_attributes.c_oflag &= ~(OPOST);
+    raw_attributes.c_cflag |= (CS8);
+    raw_attributes.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+    raw_attributes.c_cc[VMIN] = 0;
+    raw_attributes.c_cc[VTIME] = 1;
+
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw_attributes) == -1) 
+        killProgram("tcsetattr");
 }
+
+/* Init */
 
 int main() {
     enableRawmode();
-
-    char c;
     
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
+    while (1) {
+        char c = '\0';
+
+        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN)
+            killProgram("read");
+
+        if(iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+
+        if(c == 'q') break;
+    }
     
     return 0;
 }
